61e8fac86a3777a9667e0121772208d9cd4bde3d
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-03-18 18:15:13 +0100

    Log in users with a nickname

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var server = express();
 
 var users = [];
 server.set('users', users);
+server.use(express.bodyParser());
+
+server.post('/signin', function(req, res) {
+  users.push(req.body.nick);
+  res.send(200);
+});
 
 module.exports.server = server;
 
--- tests/presence_api_test.js
+++ tests/presence_api_test.js
@@ -10,6 +10,12 @@ describe('Server', function() {
       expect(server.get('users')).to.be.empty;
     });
 
+    it('should have foo logged in', function(done) {
+      request.post('http://localhost:3000/signin', {form: {nick: 'foo'}}, function() {
+        expect(server.get('users')).to.eql(['foo'])
+        done();
+      });
+    });
   });
 });
 
