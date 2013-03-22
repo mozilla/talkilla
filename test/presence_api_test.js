636281401b91897cf9c28246b643a22ed5679ef0
Florian Quèze <florian@queze.net>
2013-03-22 12:13:23 +0100

    Use double quotes to avoid ugly escaping in presence_api_test.js.

--- test/presence_api_test.js
+++ test/presence_api_test.js
@@ -1,12 +1,12 @@
-var expect = require('chai').expect;
-var request = require('request');
-var server = require('../server').server;
-var findNewNick = require('../server').findNewNick;
+var expect = require("chai").expect;
+var request = require("request");
+var server = require("../server").server;
+var findNewNick = require("../server").findNewNick;
 
 var connection;
 
-describe('Server', function() {
-  describe('presence', function() {
+describe("Server", function() {
+  describe("presence", function() {
 
     beforeEach(function() {
       connection = server.listen(3000);
@@ -16,29 +16,29 @@ describe('Server', function() {
       connection.close();
     });
 
-    it('should have no users logged in', function() {
-      expect(server.get('users')).to.be.empty;
+    it("should have no users logged in", function() {
+      expect(server.get("users")).to.be.empty;
     });
 
-    it('should have foo logged in', function(done) {
-      request.post('http://localhost:3000/signin', {form: {nick: 'foo'}}, function() {
-        expect(server.get('users')).to.eql(['foo']);
+    it("should have foo logged in", function(done) {
+      request.post("http://localhost:3000/signin", {form: {nick: "foo"}}, function() {
+        expect(server.get("users")).to.eql(["foo"]);
         done();
       });
     });
 
-    it('should have no users logged in', function(done) {
-      request.post('http://localhost:3000/signin', {form: {nick: 'foo'}}, function() {
-        request.post('http://localhost:3000/signout', {form: {nick: 'foo'}}, function() {
-          expect(server.get('users')).to.be.empty;
+    it("should have no users logged in", function(done) {
+      request.post("http://localhost:3000/signin", {form: {nick: "foo"}}, function() {
+        request.post("http://localhost:3000/signout", {form: {nick: "foo"}}, function() {
+          expect(server.get("users")).to.be.empty;
           done();
         });
       });
     });
 
-    it('should return the user\'s nick', function(done) {
-      var nick1 = 'foo';
-      request.post('http://localhost:3000/signin', {form: {nick: nick1}}, function(err, res, body) {
+    it("should return the user's nick", function(done) {
+      var nick1 = "foo";
+      request.post("http://localhost:3000/signin", {form: {nick: nick1}}, function(err, res, body) {
         var data = JSON.parse(body);
         expect(data.nick).to.eql(nick1);
         expect(data.users).to.be.empty;
@@ -46,17 +46,17 @@ describe('Server', function() {
       });
     });
 
-    it('should fix the user\'s nick if it already exists', function(done) {
-      var nick1 = 'foo';
-      request.post('http://localhost:3000/signin', {form: {nick: nick1}}, function(err, res, body) {
-        request.post('http://localhost:3000/signin', {form: {nick: nick1}}, function(err, res, body) {
+    it("should fix the user's nick if it already exists", function(done) {
+      var nick1 = "foo";
+      request.post("http://localhost:3000/signin", {form: {nick: nick1}}, function(err, res, body) {
+        request.post("http://localhost:3000/signin", {form: {nick: nick1}}, function(err, res, body) {
           expect(JSON.parse(body).nick).to.eql(findNewNick(nick1));
           done();
       });
       });
     });
 
-    it('should preserve existing characters of the nick when finding a new one', function() {
+    it("should preserve existing characters of the nick when finding a new one", function() {
       var testNicks = {
         "foo": "foo1",
         "foo1": "foo2",
@@ -77,11 +77,11 @@ describe('Server', function() {
         expect(findNewNick(nick)).to.equal(testNicks[nick]);
     });
 
-    it('should return existing users', function(done) {
-      var nick1 = 'foo';
-      var nick2 = 'bar';
-      request.post('http://localhost:3000/signin', {form: {nick: nick1}}, function() {
-        request.post('http://localhost:3000/signin', {form: {nick: 'bar'}}, function(err, res, body) {
+    it("should return existing users", function(done) {
+      var nick1 = "foo";
+      var nick2 = "bar";
+      request.post("http://localhost:3000/signin", {form: {nick: nick1}}, function() {
+        request.post("http://localhost:3000/signin", {form: {nick: nick2}}, function(err, res, body) {
           var data = JSON.parse(body);
           expect(data.nick).to.eql(nick2);
           expect(data.users).to.eql([nick1]);
