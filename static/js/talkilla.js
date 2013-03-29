648ff968cfa07f9dcc400f1082647f0cf71ff5de
Nicolas Perriault <nperriault@gmail.com>
2013-03-29 19:17:21 +0100

    click nickname to start a call
    - basic RTC object
    - click on a username to initiate a call
    - new CallView to display the user's video
    
    Quite much a work in progress

--- static/js/talkilla.js
+++ static/js/talkilla.js
@@ -1,6 +1,6 @@
-/*global jQuery, Backbone*/
+/*global jQuery, Backbone, RTC*/
 /* jshint unused: false */
-var Talkilla = (function($, Backbone) {
+var Talkilla = (function($, Backbone, RTC) {
   "use strict";
   var app = {data: {}};
 
@@ -38,21 +38,39 @@ var Talkilla = (function($, Backbone) {
 
   app.Router = Backbone.Router.extend({
     routes: {
-      '': 'index'
+      'call/:with': 'call',
+      '*actions':   'index'
     },
 
-    index: function() {
+    updateViews: function() {
       // login form
       if (this.loginView)
         this.loginView.undelegateEvents();
       this.loginView = new app.LoginView(app.data);
       this.loginView.render();
-
       // users list
       if (this.usersView)
         this.usersView.undelegateEvents();
       this.usersView = new app.UsersView(app.data);
       this.usersView.render();
+      // call view
+      if (this.callView)
+        this.callView.undelegateEvents();
+      this.callView = new app.CallView(app.data);
+      this.callView.render();
+    },
+
+    index: function() {
+      this.updateViews();
+    },
+
+    call: function(callee) {
+      if (!app.data.user) {
+        alert('Please join for calling someone');
+        return this.navigate('', {trigger: true, replace: true});
+      }
+      app.data.callee = app.data.users.findWhere({nick: callee});
+      this.updateViews();
     }
   });
 
@@ -91,7 +109,10 @@ var Talkilla = (function($, Backbone) {
       else
         $('#invite').hide();
       this.collection.each(function(user) {
-        $list.append($('<li/>').text(user.get('nick')));
+        $list.append(
+          $('<li/>').append(
+            $('<a/>').attr('href', '#call/' + user.get('nick'))
+                     .text(user.get('nick'))));
       });
       return this;
     }
@@ -111,12 +132,11 @@ var Talkilla = (function($, Backbone) {
 
     render: function() {
       if (!this.user) {
-        this.$el.find('#signin').show();
-        this.$el.find('#signout').hide().find('.nick').text('');
+        this.$('#signin').show();
+        this.$('#signout').hide().find('.nick').text('');
       } else {
-        this.$el.find('#signin').hide();
-        this.$el.find('#signout').show().find('.nick')
-                .text(this.user.get('nick'));
+        this.$('#signin').hide();
+        this.$('#signout').show().find('.nick').text(this.user.get('nick'));
       }
       return this;
     },
@@ -140,13 +160,50 @@ var Talkilla = (function($, Backbone) {
       logout(function(err) {
         if (err)
           return alert(err);
-        app.data.user = app.data.users = undefined;
+        delete app.data.callee;
+        delete app.data.user;
         app.router.index();
       });
     }
   });
 
+  app.CallView = Backbone.View.extend({
+    el: '#call',
+
+    initialize: function(options) {
+      this.user = options && options.user;
+      this.callee = options && options.callee;
+    },
+
+    render: function() {
+      if (!this.callee) {
+        this.$el.hide();
+        return this;
+      }
+      this.$('h2').text('Calling ' + this.callee.get('nick'));
+      RTC.getUserMedia(
+        {video: true, audio: true},
+
+        function onSuccess(stream) {
+          // TODO: most of this could be handled by a dedicated view
+          var video = document.createElement('video');
+          video.mozSrcObject = video.src = stream;
+          this.$('.video').html(video);
+          video.play();
+          this.$el.show();
+        }.bind(this),
+
+        function onError(err) {
+          alert("Impossible to access your webcam/microphone");
+        });
+      return this;
+    }
+  });
+
+  if (!RTC.check()) {
+    return alert("Your browser doesn't support getUserMedia, game over.");
+  }
   app.router = new app.Router();
   Backbone.history.start();
   return app;
-})(jQuery, Backbone);
+})(jQuery, Backbone, RTC);
