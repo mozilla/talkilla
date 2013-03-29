fc6404f89370a6bf8d22dfeb1a0925bb6abbe59c
Nicolas Perriault <nperriault@gmail.com>
2013-03-29 19:17:21 +0100

    click nickname to initiate a call
    - added UserEntryView
    - added user icons in connected people list
    - added events for sign in & sign out
    - added new CallView to display the user's video

--- static/js/talkilla.js
+++ static/js/talkilla.js
@@ -1,8 +1,15 @@
-/*global jQuery, Backbone*/
+/*global jQuery, Backbone, _*/
 /* jshint unused: false */
-var Talkilla = (function($, Backbone) {
+var Talkilla = (function($, Backbone, _) {
   "use strict";
   var app = {data: {}};
+  _.extend(app, Backbone.Events);
+
+  function debug() {
+    try {
+      console.log.apply(console, arguments);
+    } catch (e) {}
+  }
 
   function login(nick, cb) {
     $.ajax({
@@ -38,21 +45,40 @@ var Talkilla = (function($, Backbone) {
 
   app.Router = Backbone.Router.extend({
     routes: {
-      '': 'index'
+      'call/:with': 'call',
+      '*actions':   'index'
     },
 
-    index: function() {
+    updateViews: function() {
+      // TODO: if this keeps growing, refactor as a view pool
       // login form
       if (this.loginView)
         this.loginView.undelegateEvents();
       this.loginView = new app.LoginView(app.data);
       this.loginView.render();
-
       // users list
       if (this.usersView)
         this.usersView.undelegateEvents();
       this.usersView = new app.UsersView(app.data);
       this.usersView.render();
+      // call view
+      if (this.callView)
+        this.callView.undelegateEvents();
+      this.callView = new app.CallView(app.data);
+      this.callView.render();
+    },
+
+    index: function() {
+      this.updateViews();
+    },
+
+    call: function(callee) {
+      if (!app.data.user) {
+        alert('Please join for calling someone');
+        return this.navigate('', {trigger: true, replace: true});
+      }
+      app.data.callee = app.data.users.findWhere({nick: callee});
+      this.updateViews();
     }
   });
 
@@ -65,34 +91,58 @@ var Talkilla = (function($, Backbone) {
     model: app.User
   });
 
+  app.UserEntryView = Backbone.View.extend({
+    tagName: 'li',
+
+    render: function() {
+      var nick = this.model.get('nick');
+      this.$el.html($('<a/>')
+                      .attr('href', '#call/' + nick)
+                      .append([$('<i>').addClass('icon-user'), nick]));
+      return this;
+    }
+  });
+
   app.UsersView = Backbone.View.extend({
     el: '#users',
 
+    views: [],
+
+    initViews: function() {
+      this.views = [];
+      this.collection.each(function(model) {
+        this.views.push(new app.UserEntryView({model: model}));
+      }.bind(this));
+    },
+
     initialize: function(options) {
       this.collection = options && options.users;
-      if (this.collection)
+      if (this.collection) {
+        this.initViews();
         return this.render();
+      }
       this.collection = new app.UserSet();
       this.collection.fetch({
         error: function() {
           alert('Could not load connected users list');
         },
         success: function(users) {
+          this.initViews();
           this.render();
         }.bind(this)
       });
     },
 
     render: function() {
-      var $list = this.$el.find('ul');
-      $list.find('li:not([class=nav-header])').remove();
+      var userList = _.chain(this.views).map(function(view) {
+        return view.render();
+      }).pluck('el').value();
+      this.$('li:not(.nav-header)').remove();
+      this.$('ul').append(userList);
       if (app.data.user && this.collection.length === 0)
-        $('#invite').show();
+        this.$('#invite').show();
       else
-        $('#invite').hide();
-      this.collection.each(function(user) {
-        $list.append($('<li/>').text(user.get('nick')));
-      });
+        this.$('#invite').hide();
       return this;
     }
   });
@@ -111,12 +161,11 @@ var Talkilla = (function($, Backbone) {
 
     render: function() {
       if (!this.user) {
-        this.$el.find('#signin').show();
-        this.$el.find('#signout').hide().find('.nick').text('');
+        this.$('#signin').show();
+        this.$('#signout').hide().find('.nick').text('');
       } else {
-        this.$el.find('#signin').hide();
-        this.$el.find('#signout').show().find('.nick')
-                .text(this.user.get('nick'));
+        this.$('#signin').hide();
+        this.$('#signout').show().find('.nick').text(this.user.get('nick'));
       }
       return this;
     },
@@ -131,6 +180,7 @@ var Talkilla = (function($, Backbone) {
           return alert(err);
         app.data.user = user;
         app.data.users = users;
+        app.trigger('signin', user);
         app.router.index();
       });
     },
@@ -140,13 +190,80 @@ var Talkilla = (function($, Backbone) {
       logout(function(err) {
         if (err)
           return alert(err);
-        app.data.user = app.data.users = undefined;
+        delete app.data.callee;
+        delete app.data.user;
+        app.trigger('signout');
         app.router.index();
       });
     }
   });
 
+  app.CallView = Backbone.View.extend({
+    el: '#call',
+
+    local: undefined,
+
+    events: {
+      'click .btn-initiate': 'initiate',
+      'click .btn-hangup':   'hangup'
+    },
+
+    initialize: function(options) {
+      this.hangup();
+      this.user = options && options.user;
+      this.callee = options && options.callee;
+      this.local = $('#local-video').get(0);
+    },
+
+    initiate: function() {
+      // TODO:
+      // - extract the processus to some external lib?
+      // - handle asynchronicity (events?)
+      navigator.mozGetUserMedia(
+        {video: true, audio: true},
+
+        function onSuccess(stream) {
+          debug('local video enabled');
+          this.local.mozSrcObject = stream;
+          this.local.play();
+          this.$('.btn-initiate').addClass('disabled');
+          this.$('.btn-hangup').removeClass('disabled');
+        }.bind(this),
+
+        function onError(err) {
+          alert("Impossible to access your webcam/microphone");
+        });
+    },
+
+    hangup: function() {
+      if (this.local && this.local.mozSrcObject) {
+        this.local.mozSrcObject.stop();
+        this.local.mozSrcObject = null;
+      }
+      this.$('.btn-initiate').removeClass('disabled');
+      this.$('.btn-hangup').addClass('disabled');
+    },
+
+    render: function() {
+      if (!this.callee) {
+        this.$el.hide();
+        return this;
+      }
+      this.$('h2').text('Calling ' + this.callee.get('nick'));
+      this.initiate();
+      this.$el.show();
+      return this;
+    }
+  });
+
   app.router = new app.Router();
+
+  // app events
+  app.on('signout', function() {
+    // make sure any call is terminated on user signout
+    this.router.callView.hangup();
+  });
+
   Backbone.history.start();
   return app;
-})(jQuery, Backbone);
+})(jQuery, Backbone, _);
