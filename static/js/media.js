f1a9f070d9767717652da358c81e79c3b09ad263
Mark Banner <mbanner@mozilla.com>
2013-04-22 21:55:57 +0100

    Add some documentation to the public media.js functions and shorten the names of the callback variables.

--- static/js/media.js
+++ static/js/media.js
@@ -5,9 +5,22 @@
 (function(app) {
   "use strict";
 
+  /**
+   * Sets up meida and then starts a peer connection for a communication.
+   *
+   * @param  {String}   callee      The id of the person to call
+   * @param  {Object}   offer       Optional. sdp of the incoming call
+   * @param  {Object}   localVideo  The localVideo element to receive
+                                    the video
+   * @param  {Object}   remoteVideo The remoteVideo element to
+                                    display the video
+   * @param  {Function} successCb   Callback(peerConnection,
+   *                                         localVideo, remoteVideo)
+   * @param  {Function} errorCb     Callback(error)
+   */
   app.media.startPeerConnection = function(callee, offer, localVideo,
-                                           remoteVideo, successCallback,
-                                           errorCallback) {
+                                           remoteVideo, successCb,
+                                           errorCb) {
     // First of all, see if the user wants to let us use their media
     getMedia(
       localVideo,
@@ -18,21 +31,37 @@
 
         if (!offer)
           initiatePeerConnection(pc, callee, localVideo, remoteVideo,
-                                 successCallback, errorCallback);
+                                 successCb, errorCb);
         else
           joinPeerConnection(pc, callee, offer, localVideo, remoteVideo,
-                             successCallback, errorCallback);
+                             successCb, errorCb);
       },
 
-      errorCallback);
+      errorCb);
   };
 
-  app.media.addAnswerToPeerConnection = function (pc, answer, successCallback,
-                                                  errorCallback) {
+  /**
+   * Adds an sdp answer to the peer connection.
+   *
+   * @param {Object}   pc        The peer connection to add the sdp to.
+   * @param {Object}   answer    The answer sdp received
+   * @param {Function} successCb Callback()
+   * @param {Function} errorCb   Callback(err)
+   */
+  app.media.addAnswerToPeerConnection = function (pc, answer, successCb,
+                                                  errorCb) {
     pc.setRemoteDescription(new RTCSessionDescription(answer),
-                            successCallback, errorCallback);
+                            successCb, errorCb);
   };
 
+  /**
+   * Closes a peer connection. This does everything necessary to close
+   * a peer connection.
+   *
+   * @param {Object} pc          The peer connection to close
+   * @param {Object} localVideo  The localVideo element for the pc
+   * @param {Object} remoteVideo The remoteVideo element for the pc
+   */
   app.media.closePeerConnection = function (pc, localVideo, remoteVideo) {
     if (localVideo && localVideo.mozSrcObject) {
       if (pc)
@@ -49,38 +78,38 @@
   };
 
   function initiatePeerConnection(pc, callee, localVideo, remoteVideo,
-                                  successCallback, errorCallback) {
+                                  successCb, errorCb) {
     pc.createOffer(function (offer) {
       pc.setLocalDescription(offer, function () {
         app.services.initiateCall(callee, offer);
-        successCallback(pc, localVideo, remoteVideo);
+        successCb(pc, localVideo, remoteVideo);
       }, function (err) {
-        errorCallback(err);
+        errorCb(err);
       });
     }, function (err) {
-      errorCallback(err);
+      errorCb(err);
     });
   }
 
   function joinPeerConnection(pc, caller, offer, localVideo, remoteVideo,
-                              successCallback, errorCallback) {
+                              successCb, errorCb) {
     pc.setRemoteDescription(new RTCSessionDescription(offer), function () {
       pc.createAnswer(function(answer) {
         pc.setLocalDescription(answer, function() {
           app.services.acceptCall(caller, answer);
-          successCallback(pc, localVideo, remoteVideo);
+          successCb(pc, localVideo, remoteVideo);
         }, function (err) {
-          errorCallback(err);
+          errorCb(err);
         });
       }, function (err) {
-        errorCallback(err);
+        errorCb(err);
       });
     }, function (err) {
-      errorCallback(err);
+      errorCb(err);
     });
   }
 
-  function getMedia(localVideo, successCallback, errorCallback) {
+  function getMedia(localVideo, successCb, errorCb) {
     // TODO:
     // - handle asynchronicity (events?)
     navigator.mozGetUserMedia(
@@ -91,11 +120,11 @@
         localVideo.play();
         // Until Chrome implements srcObject, we can't use the
         // localVideo to obtain the stream back.
-        successCallback(localVideo, stream);
+        successCb(localVideo, stream);
       },
 
       function onError(err) {
-        errorCallback(err);
+        errorCb(err);
       }
     );
   }
