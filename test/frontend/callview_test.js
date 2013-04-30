b2826070c7d949f447fd8d73c3425dcf2fb12ddf
Dan Mosedale <dmose@meer.net>
2013-04-30 10:48:22 -0700

    Added tests for CallView; paired with tOkeshu

--- test/frontend/callview_test.js
+++ test/frontend/callview_test.js
@@ -1,4 +1,4 @@
-/* global app, chai, describe, it, beforeEach, afterEach, sinon */
+/* global $, app, chai, describe, it, beforeEach, afterEach, sinon */
 var expect = chai.expect;
 
 describe("CallView", function() {
@@ -6,41 +6,64 @@ describe("CallView", function() {
   var callView;
   var sandbox;
 
-  beforeEach(function() {
-    sandbox = sinon.sandbox.create();
-    sandbox.stub(app, "trigger");
-    sandbox.stub(app, "on");
-    app.services.on = function() {};
-  });
 
-  afterEach(function() {
-    sandbox.restore();
-  });
+  describe("#initialize", function() {
 
-    describe("#initialize", function() {
-
-        it("should attach a given call model to the view", function() {
-            call = new app.models.Call();
-            callView = new app.views.CallView({call: call});
-            expect(callView.call).to.be.an.instanceOf(app.models.Call);
-        });
-
-        it("should raise an error if we do give a call model", function() {
-            function init() {
-                new app.views.CallView();
-            }
-            expect(init).to.Throw();
-        });
-
+    beforeEach(function() {
+      sandbox = sinon.sandbox.create();
+      sandbox.stub(app, "trigger");
+      sandbox.stub(app, "on");
+      app.services.on = function() {};
     });
 
-    it("should call hangup when the hangup button is triggered", function() {
+    afterEach(function() {
+      sandbox.restore();
+    });
+
+    it("should attach a given call model to the view", function() {
+      call = new app.models.Call();
+      callView = new app.views.CallView({call: call});
+      expect(callView.call).to.be.an.instanceOf(app.models.Call);
+    });
+
+    it("should raise an error if we do give a call model", function() {
+      function init() {
+        new app.views.CallView();
+      }
+      expect(init).to.Throw();
+    });
+
+  });
+
+  describe("events", function() {
+
+    beforeEach(function() {
+      sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function() {
+      sandbox.restore();
+    });
+
+    it("should call hangup when the click event is fired on the hangup button",
+      function() {
+
         var el = $('<div><button class="btn-hangup"/></div>');
+        $("#fixtures").append(el);
         call = new app.models.Call();
+        sandbox.stub(app.views.CallView.prototype, "initialize");
+        sandbox.stub(app.views.CallView.prototype, "hangup");
         callView = new app.views.CallView({call: call, el: el});
-        sandbox.stub(callView, "hangup");
-        console.log(callView.el);
+
+        console.log(callView.el); // XXX
+
         $(el).find('button').click();
+
         sinon.assert.calledOnce(callView.hangup);
-    });
+
+        $(el).empty();
+
+      });
+  });
+
 });
