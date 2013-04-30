e41fa49a43ddccb9cc8a1e7ec2543dda90a90b53
Dan Mosedale <dmose@meer.net>
2013-04-30 10:57:07 -0700

    Misc refactoring, paired with tOkeshu

--- test/frontend/callview_test.js
+++ test/frontend/callview_test.js
@@ -21,12 +21,12 @@ describe("CallView", function() {
     });
 
     it("should attach a given call model to the view", function() {
-      call = new app.models.Call();
+      var call = sandbox.spy();
       callView = new app.views.CallView({call: call});
-      expect(callView.call).to.be.an.instanceOf(app.models.Call);
+      expect(callView.call).to.equal(call);
     });
 
-    it("should raise an error if we do give a call model", function() {
+    it("should raise an error if we don't pass a call model", function() {
       function init() {
         new app.views.CallView();
       }
@@ -47,22 +47,16 @@ describe("CallView", function() {
 
     it("should call hangup when the click event is fired on the hangup button",
       function() {
-
         var el = $('<div><button class="btn-hangup"/></div>');
         $("#fixtures").append(el);
-        call = new app.models.Call();
         sandbox.stub(app.views.CallView.prototype, "initialize");
         sandbox.stub(app.views.CallView.prototype, "hangup");
-        callView = new app.views.CallView({call: call, el: el});
-
-        console.log(callView.el); // XXX
+        callView = new app.views.CallView({el: el});
 
         $(el).find('button').click();
-
         sinon.assert.calledOnce(callView.hangup);
 
-        $(el).empty();
-
+        $("#fixtures").empty();
       });
   });
 
