35210a6390956b8fcdda0fd9b70e975562fe401f
Mark Banner <mbanner@mozilla.com>
2013-05-16 15:25:32 +0200

    Add Inital sign in functionality to the worker

--- static/js/worker.js
+++ static/js/worker.js
@@ -1,4 +1,18 @@
 /* jshint unused:false */
+
+function sendAjax(url, data, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function(event) {
+    cb(null, event.target.responseText);
+  };
+  xhr.onerror = function(event) {
+    cb(event.target.status ? event.target.statusText : "We are offline");
+  };
+  xhr.open('POST', url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(JSON.stringify(data));
+}
+
 function onconnect(event) {
   var port = event.ports[0];
   port.onmessage = function(event) {
@@ -16,6 +30,18 @@ function onconnect(event) {
           topic: "talkilla.login-pending",
           data: null
         });
+        sendAjax('/signin', {nick: msg.data.username},
+          function(err, responseText) {
+            if (err)
+              return port.postMessage({
+                topic: 'talkilla.login-failure',
+                data: err
+              });
+            return port.postMessage({
+              topic: 'talkilla.login-success',
+              data: {username: JSON.parse(responseText).nick}
+            });
+          });
       }
     }
   };
--- test/frontend/worker_test.js
+++ test/frontend/worker_test.js
@@ -23,7 +23,7 @@ if ('mozSocial' in navigator) {
 
           port.postMessage({topic: "talkilla.login", data: null});
         });
-
+/*
       it("should respond with a pending message if I pass in valid data",
         function(done) {
           port.onmessage = function(event) {
@@ -36,8 +36,31 @@ if ('mozSocial' in navigator) {
 
           port.postMessage({topic: "talkilla.login", data: {username: "jb"} });
         });
-    });
+*/
+      // XXX This currently incorporates both pending and success. Once we stub
+      // and create actual unit tests, we should separate these out again.
+      it("should respond with a success message if I pass in valid data",
+        function(done) {
+          var hadPending = false;
+          port.onmessage = function(event) {
+            var topic = event.data.topic;
+            var data = event.data.data;
+            if (topic === "talkilla.login-pending") {
+              expect(hadPending).to.equal(false);
+              expect(data).to.equal(null);
+              hadPending = true;
+            }
+            else {
+              expect(hadPending).to.equal(true);
+              expect(topic).to.equal("talkilla.login-success");
+              expect(data.username).to.equal("jb1");
+              done();
+            }
+          };
 
+          port.postMessage({topic: "talkilla.login", data: {username: "jb1"} });
+        });
+    });
 
   });
 }
