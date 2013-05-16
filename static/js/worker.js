f360e4a8184cadfb2832074299f2353fa4a0b8a4
Nicolas Perriault <nperriault@gmail.com>
2013-05-16 15:43:50 +0200

    moved login function to dedicated worker message handler

--- static/js/worker.js
+++ static/js/worker.js
@@ -1,5 +1,12 @@
 /* jshint unused:false */
 
+function error(port, message) {
+  port.postMessage({
+    topic: 'talkilla.error',
+    data: message
+  });
+}
+
 function sendAjax(url, data, cb) {
   var xhr = new XMLHttpRequest();
   xhr.onload = function(event) {
@@ -13,36 +20,46 @@ function sendAjax(url, data, cb) {
   xhr.send(JSON.stringify(data));
 }
 
+var handlers = {
+  'talkilla.login': function(port, msg) {
+    if (!msg.data || !msg.data.username) {
+      port.postMessage({
+        topic: "talkilla.login-failure",
+        data: "no username specified"
+      });
+      return;
+    }
+    else {
+      port.postMessage({
+        topic: "talkilla.login-pending",
+        data: null
+      });
+
+      sendAjax('/signin', {nick: msg.data.username},
+        function(err, responseText) {
+          if (err)
+            return port.postMessage({
+              topic: 'talkilla.login-failure',
+              data: err
+            });
+          return port.postMessage({
+            topic: 'talkilla.login-success',
+            data: {username: JSON.parse(responseText).nick}
+          });
+        });
+    }
+  }
+};
+
+
 function onconnect(event) {
   var port = event.ports[0];
+
   port.onmessage = function(event) {
     var msg = event.data;
-    if (msg.topic === "talkilla.login") {
-      if (!msg.data || !msg.data.username) {
-        port.postMessage({
-          topic: "talkilla.login-failure",
-          data: "no username specified"
-        });
-        return;
-      }
-      else {
-        port.postMessage({
-          topic: "talkilla.login-pending",
-          data: null
-        });
-        sendAjax('/signin', {nick: msg.data.username},
-          function(err, responseText) {
-            if (err)
-              return port.postMessage({
-                topic: 'talkilla.login-failure',
-                data: err
-              });
-            return port.postMessage({
-              topic: 'talkilla.login-success',
-              data: {username: JSON.parse(responseText).nick}
-            });
-          });
-      }
-    }
+    if (msg && msg.topic && msg.topic in handlers)
+      handlers[msg.topic](port, msg);
+    else
+      error(port, 'Topic is missing or unknown');
   };
 }
