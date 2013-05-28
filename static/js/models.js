1a9d24f188e2b52a812231af6851b446b33b16e8
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-05-28 13:59:31 +0200

    ChatApp: implement the Call model

--- static/js/models.js
+++ static/js/models.js
@@ -10,29 +10,23 @@
       this.state = StateMachine.create({
         initial: 'ready',
         events: [
-          {name: 'start',  from: 'ready',   to: 'pending'},
-          {name: 'accept', from: 'pending', to: 'ongoing'},
-          {name: 'hangup', from: '*',       to: 'terminated'}
+          // Call initiation scenario
+          {name: 'start',     from: 'ready',   to: 'pending'},
+          {name: 'establish', from: 'pending', to: 'ongoing'},
+
+          // Incoming call scenario
+          {name: 'incoming',  from: 'ready',   to: 'pending'},
+          {name: 'accept',    from: 'pending', to: 'ongoing'}
         ]
       });
 
-      this.start = this.state.start.bind(this.state);
-      this.accept = this.state.accept.bind(this.state);
-      this.hangup = this.state.hangup.bind(this.state);
-    },
+      this.start     = this.state.start.bind(this.state);
+      this.incoming  = this.state.incoming.bind(this.state);
+      this.accept    = this.state.accept.bind(this.state);
+      this.establish = this.state.establish.bind(this.state);
+    }
+  });
 
-    _pc: null,
-    _localStream: null,
-    _remoteStream: null,
-
-    _onHangup: function(media) {
-      media.closePeerConnection(this._pc, this._localStream,
-        this._remoteStream);
-      this._pc = null;
-      this._remoteStream = null;
-      this._localStream = null;
-      this.callee = null;
-      app.trigger('hangup_done');
     }
   });
 
--- test/frontend/chat_test.js
+++ test/frontend/chat_test.js
@@ -26,3 +26,68 @@ describe("ChatApp", function() {
   });
 });
 
+describe("Call", function() {
+
+  var call;
+
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+    call = new app.models.Call();
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
+  it("should have a state machine", function() {
+    expect(call.state).to.be.an.instanceOf(Object);
+  });
+
+  it("it should have an initial state", function() {
+    expect(call.state.current).to.equal('ready');
+  });
+
+  describe("#start", function() {
+
+    it("should change the state from ready to pending", function() {
+      call.start();
+      expect(call.state.current).to.equal('pending');
+    });
+
+    it("should raise an error if called twice", function() {
+      call.start();
+      expect(call.start).to.Throw();
+    });
+  });
+
+  describe("#incoming", function() {
+
+    it("should change the state from ready to pending", function() {
+      call.incoming();
+      expect(call.state.current).to.equal('pending');
+    });
+
+  });
+
+  describe("#accept", function() {
+
+    it("should change the state from pending to ongoing", function() {
+      call.start();
+      call.accept();
+      expect(call.state.current).to.equal('ongoing');
+    });
+
+  });
+
+  describe("#establish", function() {
+
+    it("should change the state from pending to ongoing", function() {
+      call.start();
+      call.establish();
+      expect(call.state.current).to.equal('ongoing');
+    });
+
+  });
+
+});
+
