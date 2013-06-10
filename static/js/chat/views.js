d0774d9abde8bb8d7f72b281397204ffb73aae31
Mark Banner <mbanner@mozilla.com>
2013-06-10 12:18:06 +0100

    Remove the previous work arounds for getting user id from the call messages, replacing it with the nick from the app.data.user set.

--- static/js/chat/views.js
+++ static/js/chat/views.js
@@ -111,8 +111,6 @@
   app.views.TextChatView = Backbone.View.extend({
     el: '#textchat', // XXX: uncouple the selector from this view
 
-    me: undefined,
-
     events: {
       'submit form': 'send'
     },
@@ -126,14 +124,6 @@
       this.collection = options.collection;
       this.call = options.call;
       this.media = options.call.media;
-
-      app.port.on('talkilla.call-start', function(data) {
-        this.me = data.caller; // If I'm receiving this, I'm the caller
-      }, this);
-
-      app.port.on('talkilla.call-incoming', function(data) {
-        this.me = data.callee; // If I'm receiving this, I'm the callee
-      }, this);
 
       this.collection.on('add', this.render, this);
 
@@ -155,7 +145,7 @@
       var message = $input.val().trim();
       $input.val('');
       this.collection.newEntry({
-        nick: this.me,
+        nick: app.data.user.get("nick"),
         message: message
       });
     },
