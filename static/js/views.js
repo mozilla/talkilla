8233531125b22f1a837c59fcf3eabb3712f2860d
Nicolas Perriault <nperriault@gmail.com>
2013-06-11 21:58:06 +0200

    added tests for ignored call notifications

--- static/js/views.js
+++ static/js/views.js
@@ -64,8 +64,8 @@
       }.bind(this));
 
       app.port.on('talkilla.offer-timeout', function(offerData) {
-        app.utils.notifyUI("Your call to " + offerData.data.callee +
-                           " did not go through", "error");
+        app.utils.notifyUI("The other party, " + offerData.callee +
+                           ", did not respond", "error");
       });
     },
 
