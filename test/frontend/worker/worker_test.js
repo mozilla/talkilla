eeeba6bfa1f6c2908896ae8009934b138497a47b
Mark Banner <mbanner@mozilla.com>
2013-06-14 10:09:07 +0100

    Move serverHandlers tests out to a separate file

--- test/frontend/worker/worker_test.js
+++ test/frontend/worker/worker_test.js
@@ -5,12 +5,10 @@
    _presenceSocketOnClose, _presenceSocketSendMessage:true,
    _presenceSocketOnOpen, _signinCallback, currentUsers:true,
    browserPort:true, _currentUserData:true, UserData,
-   currentCall:true, serverHandlers, tryPresenceSocket,
+   currentCall:true, tryPresenceSocket,
    _presenceSocketReAttached, _loginExpired, _setupWebSocket,
    _ */
 /* jshint expr:true */
-/* Needed due to the use of non-camelcase in the websocket topics */
-/* jshint camelcase:false */
 var expect = chai.expect;
 
 describe('Worker', function() {
@@ -577,82 +575,6 @@ describe('Worker', function() {
 
         expect(currentCall).to.be.equal(undefined);
       });
-  });
-
-  describe("#incoming_call", function() {
-    beforeEach(function() {
-      browserPort = {postEvent: sandbox.spy()};
-    });
-
-    afterEach(function() {
-      browserPort = undefined;
-    });
-
-    it("should store the current call data", function() {
-      var data = {
-        caller: "bob",
-        callee: "alice",
-        offer: {type: "fake", sdp: "sdp" }
-      };
-      serverHandlers.incoming_call(data);
-
-      expect(currentCall).to.deep.equal({port: undefined, data: data});
-    });
-
-    it("should open a chat window", function() {
-      serverHandlers.incoming_call({});
-
-      sinon.assert.calledOnce(browserPort.postEvent);
-      sinon.assert.calledWithExactly(browserPort.postEvent,
-        'social.request-chat', "chat.html");
-    });
-  });
-
-  describe("#call_accepted", function() {
-
-    it("should post talkilla.call-establishment to the chat window",
-      function() {
-        var port = {postEvent: sinon.spy()};
-        var data = {
-          caller: "alice",
-          callee: "bob",
-          answer: { type: "fake", sdp: "sdp" }
-        };
-
-        currentCall = {port: port, data: {caller: "alice", callee: "bob"}};
-
-        serverHandlers.call_accepted(data);
-
-        sinon.assert.calledOnce(port.postEvent);
-        sinon.assert.calledWithExactly(port.postEvent,
-          'talkilla.call-establishment', data);
-      });
-  });
-
-  describe("#call_hangup", function() {
-    var callData;
-
-    beforeEach(function() {
-      handlers.postEvent = sandbox.spy();
-      callData = {
-        other: "bob"
-      };
-      currentCall = {port: {postEvent: handlers.postEvent}, data: callData};
-    });
-
-    it("should notify the chat window", function() {
-      serverHandlers.call_hangup(callData);
-
-      sinon.assert.calledOnce(handlers.postEvent);
-      sinon.assert.calledWithExactly(handlers.postEvent,
-        'talkilla.call-hangup', callData);
-    });
-
-    it("should clear the current call data", function() {
-      serverHandlers.call_hangup(callData);
-
-      expect(currentCall).to.be.equal(undefined);
-    });
   });
 
   describe("#tryPresenceSocket", function() {
