e7cc517ddf3925f235a6086c7be85e9f7e054509
Nicolas Perriault <nperriault@gmail.com>
2013-06-27 19:45:40 +0200

    updated README with instructions for setting up Python testing env

--- test/functional/driver.py
+++ test/functional/driver.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import json
+import os
 
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from user_prefs import USER_PREFS
 
+
+SELENIUM_COMMAND_EXECUTOR = os.getenv("SELENIUM_COMMAND_EXECUTOR",
+                                      "http://127.0.0.1:4444/wd/hub")
 
 class Driver(WebDriver):
     nick = None
@@ -29,21 +32,11 @@ class Driver(WebDriver):
         self.switch_to_frame("//#social-sidebar-browser")
         self.find_element_by_css_selector('#signout button').click()
 
-    # def wait_for_frame_and_switch(self, name):
-    #     def test():
-    #         try:
-    #             self.switch_to_frame(name)
-    #             return True
-    #         except TimeoutException:
-    #             return False
-    #     while not test():
-
-
 
 def create(nick=None):
     profile = FirefoxProfile()
     for pref_name in USER_PREFS.keys():
         profile.set_preference(pref_name, json.dumps(USER_PREFS[pref_name]))
-    return Driver(command_executor="http://127.0.0.1:4444/wd/hub",
+    return Driver(command_executor=SELENIUM_COMMAND_EXECUTOR,
                   desired_capabilities={"browserName": "firefox"},
                   browser_profile=profile, nick=nick)
