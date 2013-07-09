4ac47d67095ae2a0fa8be601789be9b3e4e2ef41
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-07-09 16:38:12 +0200

    Document the FileTransfer model

--- static/js/chat/models.js
+++ static/js/chat/models.js
@@ -5,8 +5,68 @@
 (function(app, Backbone) {
   "use strict";
 
+  /**
+   * FileTransfer model.
+   *
+   * Attributes:
+   * - {Integer} progress
+   *
+   * Fired when a new chunk is available.
+   * @event chunk
+   * @param {String} id the id of the file transfer
+   * @param {ArrayBuffer} chunk
+   *
+   * Fired when the transfer is complete.
+   * @event complete
+   * @param {File|Blob} file the file transfered
+   *
+   * Example:
+   *
+   * // Sender side
+   * var transfer =
+   *   new app.models.FileTransfer({file: file}, {chunkSize: 512 * 1024});
+   * transfer.on("chunk", function(id, chunk) {
+   *   sendChunk(id, chunk);
+   * });
+   * transfer.start();
+   *
+   * // Receiver side
+   * var transfer =
+   *   new app.models.FileTransfer({filename: filename, size: size});
+   * transfer.on("complete", function(blob) {
+   *   window.URL.createObjectURL(blob);
+   * });
+   * transfer.append(chunk);
+   * transfer.append(chunk);
+   * transfer.append(chunk);
+   * ...
+   *
+   */
   app.models.FileTransfer = Backbone.Model.extend({
 
+    /**
+     * Filetransfer model constructor.
+     * @param  {Object}  attributes  Model attributes
+     * @param  {Object}  options     Model options
+     *
+     * Attribues:
+     *
+     * When initiating a file tranfer
+     *
+     * - {File} file The file to transfer
+     *
+     * When receiving a file transfer
+     *
+     * - {String} filename The name of the received file
+     * - {Integer} size The size of the received file
+     *
+     * Options:
+     *
+     * When initiating a file tranfer
+     *
+     * - {Integer} chunkSize The size of the chunks
+     *
+     */
     initialize: function(attributes, options) {
       this.options = options;
       this.id = this.set("id", _.uniqueId()).id;
@@ -27,6 +87,15 @@
       this.on("chunk", this._onProgress, this);
     },
 
+    /**
+     * Turns a FileTransfer object into a JSON ready object.
+     *
+     * @return {Object} the serializable object
+     *
+     * Return value:
+     * - {String} filename The name of the file
+     * - {Integer} progress The progress of the file transfer
+     */
     toJSON: function() {
       var progress = this.get("progress");
       var json = {
@@ -40,10 +109,24 @@
       return json;
     },
 
+    /**
+     * Start the file transfer.
+     *
+     * It actually trigger the file transfer to emit chunks one after
+     * the other until the end of the file is reached.
+     */
     start: function() {
       this._readChunk();
     },
 
+    /**
+     * Append a chunk to the current file transfer.
+     *
+     * Accumulates the data until the transfer is complete.
+     * Raise an error if we append more data than expected.
+     *
+     * @param {ArrayBuffer} chunk the chunk to append
+     */
     append: function(chunk) {
       this.chunks.push(chunk);
       this.seek += chunk.byteLength;
