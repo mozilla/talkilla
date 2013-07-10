359dfe8bcb6e777a933c7ad38b6a76ef581cffa7
Dan Mosedale <dmose@meer.net>
2013-07-10 16:27:41 -0700

    Make run_selenium_test.sh pass args through to python; remove cruft.

--- bin/run_selenium_test.sh
+++ bin/run_selenium_test.sh
@@ -5,7 +5,6 @@ DIR=`dirname $0`
 set -x
 
 $DIR/selenium.sh start
-# ./node_modules/mocha/bin/mocha $@ --reporter spec
 
 # activate sets up the python virtual environment, and does a lot of stuff
 # so we turn off command echoing while it's sourcing
@@ -13,7 +12,7 @@ set +x
 . ./.venv/bin/activate
 set -x
 
-python test/functional/test.py
+python $@
 RESULT=$?
 $DIR/selenium.sh stop
 
