42008a47fafbd437cfc2ec3bb1f9f773f579613b
Dan Mosedale <dmose@meer.net>
2013-07-10 15:38:38 -0700

    Fix PEP blank lines warning

--- test/functional/test.py
+++ test/functional/test.py
@@ -7,6 +7,7 @@ import signal
 import subprocess
 import unittest
 
+
 class BrowserTest(unittest.TestCase):
     def setUp(self):
         super(BrowserTest, self).setUp()
@@ -51,7 +52,8 @@ class SingleBrowserTest(mixins.WithBob, BrowserTest):
         self.bob.quit()
         self.bob.start_session(capabilities)
 
-        # inject the cookies into sidebar domain, as they include our login state
+        # inject the cookies into sidebar domain, as they include our login
+        # state
         self.bob.switch_to_frame("//#social-sidebar-browser")
         for cookie in cookies:
             self.bob.add_cookie(cookie)
@@ -61,36 +63,37 @@ class SingleBrowserTest(mixins.WithBob, BrowserTest):
 
         assert self.bob.isSignedIn()
 
+
 class MultipleBrowsersTest(mixins.WithBob, mixins.WithLarry, BrowserTest):
     def test_signin_users(self):
-         # Sign in user 1
-         self.bob.signin()
-         assert self.bob.find_element_by_css_selector("strong.nick").text == "bob"
-         assert self.bob.find_element_by_id("signout").is_displayed()
+        # Sign in user 1
+        self.bob.signin()
+        assert self.bob.find_element_by_css_selector("strong.nick").text == "bob"
+        assert self.bob.find_element_by_id("signout").is_displayed()
 
-         # Check there is a message that this is the only person logged in
-         assert "only person" in self.bob.find_element_by_css_selector(".alert-info").text
+        # Check there is a message that this is the only person logged in
+        assert "only person" in self.bob.find_element_by_css_selector(".alert-info").text
 
-         # Sign in user 2
-         self.larry.signin()
-         assert self.larry.find_element_by_css_selector("strong.nick").text == "larry"
+        # Sign in user 2
+        self.larry.signin()
+        assert self.larry.find_element_by_css_selector("strong.nick").text == "larry"
 
-         # Check that both pages no longer have the alert on them
-         assert len(self.bob.find_elements_by_css_selector(".alert-info")) == 0
-         assert len(self.larry.find_elements_by_css_selector(".alert-info")) == 0
+        # Check that both pages no longer have the alert on them
+        assert len(self.bob.find_elements_by_css_selector(".alert-info")) == 0
+        assert len(self.larry.find_elements_by_css_selector(".alert-info")) == 0
 
-         # Sign out user 1
-         self.bob.signout()
-         assert len(self.bob.find_elements_by_css_selector(".alert-info")) == 0
-         assert not self.bob.find_element_by_id("signout").is_displayed()
+        # Sign out user 1
+        self.bob.signout()
+        assert len(self.bob.find_elements_by_css_selector(".alert-info")) == 0
+        assert not self.bob.find_element_by_id("signout").is_displayed()
 
-         # Check there's an alert on user 2's screen
-         assert "only person" in self.larry.find_element_by_css_selector(".alert-info").text
+        # Check there's an alert on user 2's screen
+        assert "only person" in self.larry.find_element_by_css_selector(".alert-info").text
 
-         # Now sign out user 2
-         self.larry.signout()
-         assert len(self.larry.find_elements_by_css_selector(".alert-info")) == 0
-         assert not self.larry.find_element_by_id("signout").is_displayed()
+        # Now sign out user 2
+        self.larry.signout()
+        assert len(self.larry.find_elements_by_css_selector(".alert-info")) == 0
+        assert not self.larry.find_element_by_id("signout").is_displayed()
 
     def test_chat_window(self):
         # Sign both users in
