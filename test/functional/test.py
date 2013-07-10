fe0e085c06e42b52c07ff3c2bfcb9c5a0f1a1c81
Dan Mosedale <dmose@meer.net>
2013-07-10 14:22:06 -0700

    Implement login persistence of reload test, and fix login persistence over refresh test

--- test/functional/test.py
+++ test/functional/test.py
@@ -32,9 +32,31 @@ class SingleBrowserTest(mixins.WithBob, BrowserTest):
     # checks that even if the user reloads the sidebar from the context menu,
     # she'll remain logged in.
     def test_login_persistence_over_reload(self):
+        self.bob.switch_to_frame("//#social-sidebar-browser")
         self.bob.signin()
+
+        self.bob.refresh()
+
         assert self.bob.isSignedIn()
 
+    # test that the user remains logged in across browser restarts
+    def test_login_persistence_over_restart(self):
+        self.bob.signin()
+
+        # save off session & profile state for creation of new browser env
+        cookies = self.bob.get_cookies()
+        capabilities = self.bob.desired_capabilities
+
+        # get a new browser
+        self.bob.quit()
+        self.bob.start_session(capabilities)
+
+        # inject the cookies into sidebar domain, as they include our login state
+        self.bob.switch_to_frame("//#social-sidebar-browser")
+        for cookie in cookies:
+            self.bob.add_cookie(cookie)
+
+        # force a refresh so that the page sees the cookies
         self.bob.refresh()
 
         assert self.bob.isSignedIn()
