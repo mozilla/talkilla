44d68b6559dc5201f5f96288eb772948c3122b40
Dan Mosedale <dmose@meer.net>
2013-07-11 14:00:38 -0700

    Split up test.py into a base class a multiple test files for autodiscovery

---
+++ test/functional/BrowserTest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import signal
+import subprocess
+import unittest
+
+
+class BrowserTest(unittest.TestCase):
+    def setUp(self):
+        super(BrowserTest, self).setUp()
+        app_cmd = 'PORT=3000 NODE_ENV=test node app.js'
+        self.node_app = subprocess.Popen(app_cmd, stdout=subprocess.PIPE,
+                                         shell=True, preexec_fn=os.setsid)
+
+    def tearDown(self):
+        os.killpg(self.node_app.pid, signal.SIGTERM)
+        self.node_app.kill()
+        # ps aux|grep "node app"|grep -v grep|awk '{print $2}'|xargs kill
