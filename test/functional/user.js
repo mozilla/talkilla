266071b302e83e9589662761f0e1b260e3b488bb
Mark Banner <mbanner@mozilla.com>
2013-07-14 18:58:30 +0100

    Disable permissions prompt for media, and also override the default ice servers, so there are no network requirements for the tests

--- test/functional/user.js
+++ test/functional/user.js
@@ -1,3 +1,6 @@
+user_pref("media.navigator.permission.disabled", true);
+user_pref("media.peerconnection.default_iceservers", "[]");
+user_pref("media.peerconnection.use_document_iceservers", false);
 user_pref("social.activeProviders", "{\"http://localhost:3000\":1}");
 user_pref("social.enabled", true);
 user_pref("social.manifest.tests", "{\"name\":\"Talkilla Functional tests\",\"iconURL\":\"http://localhost:3000/icon.png\",\"sidebarURL\":\"http://localhost:3000/sidebar.html\",\"workerURL\":\"http://localhost:3000/js/worker.js\",\"origin\":\"http://localhost:3000\",\"enabled\":true,\"last_modified\":135101330568}");
