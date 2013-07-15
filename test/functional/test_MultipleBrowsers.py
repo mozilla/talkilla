ffca58b5a806938a12cbf186c9b753feb4c9b0c2
Mark Banner <mbanner@mozilla.com>
2013-07-15 17:23:51 +0200

    Move video test back to multiple browsers file for now

--- test/functional/test_MultipleBrowsers.py
+++ test/functional/test_MultipleBrowsers.py
@@ -55,5 +55,33 @@ class MultipleBrowsersTest(mixins.WithBob, mixins.WithLarry,
         # Bob checks for his own chat window
         self.bob.switch_to_frame("//chatbox")
 
+    def test_video_call(self):
+        # Sign both users in
+        self.bob.signin()
+        self.larry.signin()
+
+        # Bob opens Larry's conversation window
+        self.bob.openConversation()
+
+        # Bob calls Larry
+        self.bob.startCall(True)
+
+        # Bob sees the outgoing call
+        assert self.bob.find_element_by_css_selector(
+            ".outgoing-text").is_displayed()
+
+        # Larry gets a window for receiving the call
+        self.larry.switch_to_frame("//chatbox")
+
+        # Larry accepts the call
+        self.larry.acceptCall()
+
+        # Larry sees the call
+        assert self.larry.find_element_by_id("call").is_displayed()
+
+        # Bob sees the call
+        assert self.bob.find_element_by_id("call").is_displayed()
+
+
 if __name__ == "__main__":
     unittest.main(catchbreak=True)
