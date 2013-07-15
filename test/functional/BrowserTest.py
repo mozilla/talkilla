6d2517d69d0adda1a71164cce840e7bf0026cd58
Dan Mosedale <dmose@meer.net>
2013-07-15 17:45:45 +0200

    Get rid of setsid to improve node cleanup robustness

--- test/functional/BrowserTest.py
+++ test/functional/BrowserTest.py
@@ -19,8 +19,7 @@ class BrowserTest(unittest.TestCase):
         app_cmd = 'PORT=3000 NO_LOCAL_CONFIG=true NODE_ENV=test node app.js'
         try:
             self.__class__.node_app = subprocess.Popen(
-                app_cmd, stdout=subprocess.PIPE, shell=True,
-                preexec_fn=os.setsid)
+                app_cmd, stdout=subprocess.PIPE, shell=True)
         finally:
             # ensure that we cleanup even if something weird happened
             self.addCleanup(kill_app, self.__class__.node_app)
