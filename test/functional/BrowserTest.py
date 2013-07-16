c88621fcb51efd320b426bf2e8892c52310388a1
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-07-16 17:05:29 +0200

    Fix PEP8 errors

--- test/functional/BrowserTest.py
+++ test/functional/BrowserTest.py
@@ -17,10 +17,11 @@ class BrowserTest(unittest.TestCase):
     def setUp(self):
         cmd = ("node", "app.js")
         env = os.environ.copy()
-        env.update({"PORT": "3000", "NO_LOCAL_CONFIG": "true", "NODE_ENV": "test"})
+        env.update({"PORT": "3000",
+                    "NO_LOCAL_CONFIG": "true",
+                    "NODE_ENV": "test"})
         self.node_app = subprocess.Popen(cmd, env=env)
         self.addCleanup(kill_app, self.node_app)
 
     def tearDown(self):
         kill_app(self.node_app)
-
