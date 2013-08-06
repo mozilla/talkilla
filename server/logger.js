6af3ad41838cda2c608a7f19ba0065c06030d56d
Mark Banner <mbanner@mozilla.com>
2013-08-06 16:57:37 +0100

    For production, log at info level for now

--- server/logger.js
+++ server/logger.js
@@ -3,7 +3,7 @@ var bunyan = require('bunyan');
 // Logging
 var logger = bunyan.createLogger({
   name: 'talkilla',
-  level: "error", // XXX: should use app.get("config")
+  level: "info", // XXX: should use app.get("config")
   serializers: {err: bunyan.stdSerializers.err}
 });
 
