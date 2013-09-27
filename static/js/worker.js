2616b6d7adbadeba959307c7213c44aac364ba7d
Nicolas Perriault <nperriault@gmail.com>
2013-09-27 21:20:19 +0200

    don't fail on contacts db being unavalaible
    just log the event

--- static/js/worker.js
+++ static/js/worker.js
@@ -596,18 +596,23 @@ _setupServer(server);
 
 function loadContacts(cb) {
   contactsDb.all(function(err, records) {
-    if (err)
-      return ports.broadcastError(err);
+    if (err) {
+      contacts = [];
+      ports.broadcastError(err);
+    } else {
+      contacts = records;
+    }
 
-    contacts = records;
     contacts.forEach(function(userId) {
       if (!Object.prototype.hasOwnProperty.call(currentUsers, userId))
         currentUsers[userId] = {presence: "disconnected"};
     });
+
     // We need to broadcast the list in case we've been slow loading
     // the database and the initial presence list has already been
     // broadcast.
     ports.broadcastEvent('talkilla.users', getCurrentUsersArray());
+
     // callback is mostly useful for tests
     if (typeof cb === "function")
       cb();
