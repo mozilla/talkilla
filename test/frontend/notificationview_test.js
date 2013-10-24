3bb56597043eef030384b08e1ea6b67e3908aaf6
Nicolas Perriault <nperriault@gmail.com>
2013-10-24 18:32:57 +0200

    added missing tests for NotificationView

---
+++ test/frontend/notificationview_test.js
@@ -0,0 +1,46 @@
+/*global chai, app, sinon */
+var expect = chai.expect;
+
+describe("app.views", function() {
+  "use strict";
+
+  var sandbox;
+
+  describe("app.views.Notification", function() {
+    beforeEach(function() {
+      sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function() {
+      sandbox.restore();
+    });
+
+    describe("#clear", function() {
+      it("should clear notification", function() {
+        var view = new app.views.NotificationView({
+          model: new app.models.Notification({type: "plip", message: "plop"})
+        });
+        sandbox.stub(view, "undelegateEvents");
+        sandbox.stub(view, "remove");
+
+        view.clear();
+
+        sinon.assert.calledOnce(view.undelegateEvents);
+        sinon.assert.calledOnce(view.remove);
+      });
+    });
+
+    describe("#render", function() {
+      it("should render itself", function() {
+        var view = new app.views.NotificationView({
+          model: new app.models.Notification({type: "plip", message: "plop"})
+        });
+
+        view.render();
+
+        expect(view.$("div").attr("class")).eql("alert alert-plip");
+        expect(view.$el.text()).to.match(/plop/);
+      });
+    });
+  });
+});
