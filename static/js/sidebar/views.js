443c99ba4a65fb52ccf4d15b674288b2ade35f16
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-12-02 15:23:18 +0100

    Improve the iframe destruction and creation flow

--- static/js/sidebar/views.js
+++ static/js/sidebar/views.js
@@ -231,11 +231,15 @@
 
     render: function() {
       if (!this.appStatus.get('workerInitialized')) {
-        this.$('#signin').hide();
         this.$('#signout').hide();
         this.$('[name="spa-setup"]').remove();
       } else if (!this.user.get("nick")) {
-        this.$('#signin').show();
+        var iframe = $("<iframe>")
+          .attr("src", "/talkilla-spa-setup.html")
+          .attr("id", "signin")
+          .attr("name", "spa-setup");
+        $("#login p:first").append(iframe);
+
         this.$('#signout').hide().find('.nick').text('');
       } else {
         this.$('#signin').hide();
