7e3a7648df2b319b841c03bfa5afb9269a172fbb
Dan Mosedale <dmose@meer.net>
2013-12-10 15:16:34 -0800

    Hoist appview events tests and sandbox stuff out of #initialize block

--- test/frontend/sidebar/appview_test.js
+++ test/frontend/sidebar/appview_test.js
@@ -6,20 +6,21 @@ var expect = chai.expect;
 describe("AppView", function() {
   var sandbox;
 
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+
+    sandbox.stub(app.views, "NotificationsView");
+    sandbox.stub(app.views, "LoginView");
+    sandbox.stub(app.views, "UsersView");
+    sandbox.stub(app.views, "ImportContactsView");
+    sandbox.stub(app.views, "SPAView");
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
   describe("#initialize", function() {
-    beforeEach(function() {
-      sandbox = sinon.sandbox.create();
-
-      sandbox.stub(app.views, "NotificationsView");
-      sandbox.stub(app.views, "LoginView");
-      sandbox.stub(app.views, "UsersView");
-      sandbox.stub(app.views, "ImportContactsView");
-      sandbox.stub(app.views, "SPAView");
-    });
-
-    afterEach(function() {
-      sandbox.restore();
-    });
 
     describe("required parameters", function() {
       it("should require an appStatus parameter", function() {
@@ -77,63 +78,64 @@ describe("AppView", function() {
       });
     });
 
-    describe("events", function() {
+  });
 
-      var appViewOptions;
+  describe("events", function() {
 
-      beforeEach(function() {
-        var $el = $("<div id='appViewDiv'>" +
-                    "  <nav>" +
-                    "    <ul>" +
-                    "      <li>" +
-                    "        <a class='close-panel-on-click'></a>" +
-                    "      </li>" +
-                    "    </ul>" +
-                    "  </nav>" +
-                    "</div>");
-        $("#fixtures").append($el);
-        appViewOptions = {
-          el: $el.get()[0],
-          user: {},
-          users: [],
-          appStatus: [],
-          spa: {}
-        };
-        sandbox.stub(window, "close");
+    var appViewOptions;
+
+    beforeEach(function() {
+      var $el = $("<div id='appViewDiv'>" +
+        "  <nav>" +
+        "    <ul>" +
+        "      <li>" +
+        "        <a class='close-panel-on-click'></a>" +
+        "      </li>" +
+        "    </ul>" +
+        "  </nav>" +
+        "</div>");
+      $("#fixtures").append($el);
+      appViewOptions = {
+        el: $el.get()[0],
+        user: {},
+        users: [],
+        appStatus: [],
+        spa: {}
+      };
+      sandbox.stub(window, "close");
+    });
+
+    afterEach(function() {
+      $("#fixtures").empty();
+    });
+
+    describe("click", function() {
+      it("should call window.close when fired on a.close-panel-on-click" +
+        " if the appView is not running in a sidebar", function() {
+
+        appViewOptions.isInSidebar = false;
+        var appView = new app.views.AppView(appViewOptions);
+
+        var $link = appView.$("a.close-panel-on-click");
+        $link.trigger("click");
+
+        sinon.assert.calledOnce(window.close);
+        sinon.assert.calledWithExactly(window.close);
       });
 
-      afterEach(function() {
-        $("#fixtures").empty();
+      it("should not call window.close when fired on a.close-panel-on-click" +
+        " if the appView is running in a sidebar", function() {
+
+        appViewOptions.isInSidebar = true;
+        var appView = new app.views.AppView(appViewOptions);
+
+        var $link = appView.$("a.close-panel-on-click");
+        $link.trigger("click");
+
+        sinon.assert.notCalled(window.close);
       });
-
-      describe("click", function() {
-        it("should call window.close when fired on a.close-panel-on-click" +
-          " if the appView is not running in a sidebar", function() {
-
-          appViewOptions.isInSidebar = false;
-          var appView = new app.views.AppView(appViewOptions);
-
-          var $link = appView.$("a.close-panel-on-click");
-          $link.trigger("click");
-
-          sinon.assert.calledOnce(window.close);
-          sinon.assert.calledWithExactly(window.close);
-        });
-
-        it("should not call window.close when fired on a.close-panel-on-click" +
-          " if the appView is running in a sidebar", function() {
-
-          appViewOptions.isInSidebar = true;
-          var appView = new app.views.AppView(appViewOptions);
-
-          var $link = appView.$("a.close-panel-on-click");
-          $link.trigger("click");
-
-          sinon.assert.notCalled(window.close);
-        });
-      });
-
     });
 
   });
+
 });
