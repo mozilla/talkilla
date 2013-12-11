ccb12e5205d68e8d829812dda078fd7bf86fefe3
Nicolas Perriault <nperriault@gmail.com>
2013-12-11 11:13:43 +0100

    refactored BaseView dependency mngmt system, cleaned code

--- static/js/views.js
+++ static/js/views.js
@@ -31,45 +31,62 @@
      * Constructs this view, checking and attaching required dependencies.
      */
     constructor: function(options) {
-      this._processDependencies(options || {});
+      if (Object.keys(this.dependencies || {}).length > 0) {
+        this._checkRequiredProperties(options || {});
+        this._checkRequiredTypes(options || {});
+      }
       Backbone.View.apply(this, arguments);
     },
 
     /**
-     * Checks an options object for required dependencies and attaches them to
-     * current instance.
-     * @param  {Object}   options   Options object
-     * @throws {DependencyError} If object doesn't own an expected property
+     * Checks if any of an Object values matches any of current dependency type
+     * requirements.
+     * @param  {Object} object
+     * @throws {DependencyError}
      */
-    _processDependencies: function(options) {
-      /*jshint eqnull:true*/
-      var dependencyNames = Object.keys(this.dependencies || {});
-      if (dependencyNames.length === 0)
-        return;
-      var diff = _.difference(dependencyNames,
-                              Object.keys(options).filter(function(name) {
-                                return options[name] != null;
-                              }));
-      if (diff.length > 0)
-        throw new DependencyError("missing required " + diff.join(", "));
-      dependencyNames.forEach(function(name) {
-        var types = this.dependencies[name], option = options[name];
+    _checkRequiredTypes: function(object) {
+      Object.keys(this.dependencies || {}).forEach(function(name) {
+        var types = this.dependencies[name];
         types = Array.isArray(types) ? types : [types];
-        var match = types.some(function(Type) {
-          /*jshint eqeqeq:false*/
-          return typeof Type === "undefined" ||        // skip checking
-                 option.constructor == Type  ||        // native types
-                 Type.prototype.isPrototypeOf(option); // custom types
-        });
-        if (!match) {
+        if (!this._dependencyMatchTypes(object[name], types)) {
           throw new DependencyError(
             "invalid dependency: " + name + "; expected " +
-            types.map(function(type) {
-              return type && type.name;
-            }).join(", "));
+            types.map(function(type) { return type && type.name; }).join(", "));
         }
-        this[name] = option;
+        this[name] = object[name];
       }, this);
+    },
+
+    /**
+     * Checks if an Object owns the required keys defined in dependencies.
+     * @param  {Object} object
+     * @throws {DependencyError}
+     */
+    _checkRequiredProperties: function(object) {
+      /*jshint eqnull:true*/
+      var diff = _.difference(Object.keys(this.dependencies || {}),
+                              Object.keys(object)
+                  .filter(function(name) {
+                    return object[name] != null;
+                  }));
+      if (diff.length > 0)
+        throw new DependencyError("missing required " + diff.join(", "));
+    },
+
+    /**
+     * Checks if a given value matches any of the provided type requirements.
+     * @param  {Object} value  The value to check
+     * @param  {Array}  types  The list of types to check the value against
+     * @return {Boolean}
+     * @throws {DependencyError} If the value doesn't match any types.
+     */
+    _dependencyMatchTypes: function(value, types) {
+      return types.some(function(Type) {
+        /*jshint eqeqeq:false*/
+        return typeof Type === "undefined" ||       // skip checking
+               value.constructor == Type   ||       // native types
+               Type.prototype.isPrototypeOf(value); // custom types
+      });
     }
   });
 
